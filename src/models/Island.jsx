8a0bde39ee48bc75baf54ad21a91a092d80e2478
Alex Moyer <alexmoyer@Alexs-MacBook-Air.local>
2024-01-12 12:20:53 -0500

    first

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -15,7 +15,7 @@ import {a} from '@react-spring/three'
 export function Island({
     isRotating,
     setIsRotating,
-    // setCurrentStage,
+    setCurrentStage,
     currentFocusPoint,
     ...props
   }) {
@@ -38,25 +38,26 @@ export function Island({
     e.stopPropagation()
     e.preventDefault()
     setIsRotating(false)
-    const clientX = e.touches ? e.touches[0].clientX : e.clientX
-    const delta = (clientX - lastX.current) / viewport.width
-    islandRef.current.rotation.y += delta * 0.01 * Math.PI
-    lastX.current = rotationSpeed.current = delta * 0.01 * Math.PI
   }
   const handlePointerMove= (e) => {
     e.stopPropagation()
     e.preventDefault()
     if (isRotating) {
-        handlePointerUp(e)
+      const clientX = e.touches ? e.touches[0].clientX : e.clientX
+      const delta = (clientX - lastX.current) / viewport.width
+      islandRef.current.rotation.y += delta * 0.001 * Math.PI
+      lastX.current = rotationSpeed.current = delta * 0.001 * Math.PI
     }
   }
   const handleKeyDown = (e) => {
     if (e.key === 'ArrowLeft') {
         if(!isRotating) setIsRotating(true)
-        islandRef.current.rotation.y += 0.01 * Math.PI
+        islandRef.current.rotation.y += 0.005 * Math.PI
+        rotationSpeed.current = 0.007
     } else if (e.key === 'ArrowRight') {
         if(!isRotating) setIsRotating(true)
-        islandRef.current.rotation.y -= 0.01 * Math.PI
+        islandRef.current.rotation.y -= 0.005 * Math.PI
+        rotationSpeed.current = -0.007
     }
   }
   const handleKeyUp = (e) => {
@@ -65,32 +66,54 @@ export function Island({
     }
   }
 
-  useFrame(() => {
+  useFrame((_, delta) => {
     if (!isRotating) {
         rotationSpeed.current *= dampingFactor
         if (Math.abs(rotationSpeed.current) < 0.001) {
             rotationSpeed.current = 0
         }
+        islandRef.current.rotation.y += rotationSpeed.current * delta
     } else {
         const rotation = islandRef.current.rotation.y
     
+        const normalizedRotation =
+        ((rotation % (2 * Math.PI)) + 2 * Math.PI) % (2 * Math.PI);
+
+      // Set the current stage based on the island's orientation
+      switch (true) {
+        case normalizedRotation >= 5.45 && normalizedRotation <= 5.85:
+          setCurrentStage(4);
+          break;
+        case normalizedRotation >= 0.85 && normalizedRotation <= 1.3:
+          setCurrentStage(3);
+          break;
+        case normalizedRotation >= 2.4 && normalizedRotation <= 2.6:
+          setCurrentStage(2);
+          break;
+        case normalizedRotation >= 4.25 && normalizedRotation <= 4.75:
+          setCurrentStage(1);
+          break;
+        default:
+          setCurrentStage(null);
+      }
     }
   })
 
   useEffect(() => {
-    document.addEventListener('pointerdown', handlePointerDown)
-    document.addEventListener('pointerup', handlePointerUp)
-    document.addEventListener('pointermove', handlePointerMove)
+    const canvas = gl.domElement
+    canvas.addEventListener('pointerdown', handlePointerDown)
+    canvas.addEventListener('pointerup', handlePointerUp)
+    canvas.addEventListener('pointermove', handlePointerMove)
     document.addEventListener('keyup', handleKeyUp)
     document.addEventListener('keydown', handleKeyDown)
     return () => {
-        document.removeEventListener('pointerdown', handlePointerDown)
-        document.removeEventListener('pointerup', handlePointerUp)
-        document.removeEventListener('pointermove', handlePointerMove)
+        canvas.removeEventListener('pointerdown', handlePointerDown)
+        canvas.removeEventListener('pointerup', handlePointerUp)
+        canvas.removeEventListener('pointermove', handlePointerMove)
         document.removeEventListener('keyup', handleKeyUp)
         document.addEventListener('keydown', handleKeyDown)
     }
-  }, [gl, handlePointerDown, handlePointerUp, handlePointerMove, handleKeyDown, handleKeyUp])
+  }, [gl, handlePointerDown, handlePointerUp, handlePointerMove])
   
 
 
